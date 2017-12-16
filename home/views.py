71e81192a00cde2c1fb1dc6422db61442dfa7d73
Lincon Dias <lincondiasribeiro@gmail.com>
2017-12-16 10:44:02 -0200

    Added login, logout views

--- home/views.py
+++ home/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.utils.http import is_safe_url
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import REDIRECT_FIELD_NAME, login as auth_login, logout as auth_logout
+from django.views.generic import FormView, RedirectView
 
-# Create your views here.
+class LoginView(FromView):
+    form_class = AuthenticationForm
+    redirect_field_name = REDIRECT_FIELD_NAME
+
+    def form_valid(self, form):
+        auth_login(self.request, form.get_user())
+        return super(LoginView, self).form_valid(form)
+
+    def get_success_url(self):
+        redirect_to = self.request.REQUEST.get(self.redirect_field_name)
+        if not is_safe_url(url=redirect_to, host=self.request.get_host()):
+            redirect_to = self.success_url
+        return redirect_to
+
+class LogoutView(RedirectView):
+    url = '/auth/login/'
+
+    def get(self, request, *args, **kwargs):
+        auth_logout(request)
+        return super(LogoutView, self).get(request, *args, **kwargs)
--- ttm/urls.py
+++ ttm/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from home.views import LoginView, LogoutView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
 ]
